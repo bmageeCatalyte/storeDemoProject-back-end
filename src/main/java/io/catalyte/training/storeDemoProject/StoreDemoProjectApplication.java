5f8532db1a8dfd7fb29fa43c5c81427cc1e5d43f
Brett Magee <bmagee@catalyte.io>
2021-09-21 21:33:14 -0500

    initial commit

---
+++ src/main/java/io/catalyte/training/storeDemoProject/StoreDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package io.catalyte.training.storeDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/catalyte/training/storeDemoProject/StoreDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.catalyte.training.storeDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
