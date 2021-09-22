018d20b0c61cdb486376034e63061decfbc45931
Brett Magee <bmagee@catalyte.io>
2021-09-21 22:14:33 -0500

    Add user model

---
+++ src/main/java/io/catalyte/training/storeDemoProject/models/User.java
@@ -0,0 +1,77 @@
+package io.catalyte.training.storeDemoProject.models;
+
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "user_table")
+public class User implements UserDetails {
+  @Id
+  private Long id;
+
+  private String username;
+  private String password;
+
+  public User() {
+  }
+
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return false;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return false;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return false;
+  }
+}
